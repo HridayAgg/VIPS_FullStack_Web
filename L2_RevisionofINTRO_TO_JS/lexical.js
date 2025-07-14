31d53d5f9e14bc62a69d03185e954d1a6398fef1
Hriday <78541894+HridayAgg@users.noreply.github.com>
2025-07-14 20:27:35 +0530

    Delete L2_RevisionofINTRO_TO_JS directory

--- L2_RevisionofINTRO_TO_JS/lexical.js
+++
@@ -1,7 +0,0 @@
-let a = 10
-let b = 50
-let c = 30
-{
-    let d=60;
-    console.log        
-}
--- L2_RevisionofINTRO_TO_JS/settimeout_func.js
+++
@@ -1,6 +0,0 @@
-for(let i=0;i<=5;i++){
-    setTimeout(function(){
-        console.log(i)}
-        ,5000)
-}
-
